7675f330dded8843c49585c6d17f4fcd10ea0963
Jessica Mosby <jessicamosby@mac.com>
2022-11-28 15:04:01 -0500

    fixed syntax error in fetch utils, login now working

--- fetch-utils.js
+++ fetch-utils.js
@@ -7,7 +7,7 @@ const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 export async function createTodo(todo) {
     // create a single incomplete todo with the correct 'todo' property for this user in supabase
     const response = await client
-        .from('todos');
+        .from('todos')
         .insert({
             todo: todo,
             complete: false,
